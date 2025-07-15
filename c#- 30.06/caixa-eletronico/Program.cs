e731da51af2df670daeae191957c80613f2d0a60
Mari1916 <mariana.freitas1916@gmail.com>
2025-07-15 15:30:18 -0300

    subindo td de uma vez

---
+++ c#- 30.06/caixa-eletronico/Program.cs
@@ -0,0 +1,131 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+double saldo = 0;
+int opcao = 0;
+
+do
+{
+    ExibirMenu();
+
+    if (int.TryParse(Console.ReadLine(), out opcao))
+    {
+        switch (opcao)
+        {
+            case 1:
+                ExibirSaldo();
+                Console.ReadKey();
+                break;
+            case 2:
+                Depositar();
+                Console.ReadKey();
+                break;
+            case 3:
+                Sacar();
+                Console.ReadKey();
+                break;
+            case 4:
+                Extrato();
+                Console.ReadKey();
+                break;
+            case 5:
+                Console.WriteLine("Sair");
+                Console.ReadKey();
+                break;
+            default:
+                Console.WriteLine("Escolha uma opção válida");
+                Console.ReadKey();
+                break;
+        }
+    }
+    else
+    {
+        Console.WriteLine("Não é uma opção válida, tente novamente");
+        Console.ReadKey();
+    }
+
+} while (opcao != 5);
+
+void ExibirMenu()
+{
+    Console.Clear();
+    Console.WriteLine("Selicione uma das opções a baixo:");
+    Console.WriteLine("1 - Saldo");
+    Console.WriteLine("2 - depositar");
+    Console.WriteLine("3 - Sacar");
+    Console.WriteLine("4 - Extrato");
+    Console.WriteLine("5 - Sair");
+}
+
+void ExibirSaldo()
+{
+    if (saldo < 0)
+    {
+        Console.WriteLine($"Saldo atual > R$ {saldo} 😰");
+    }
+    else
+    {
+        Console.WriteLine($"Saldo atual > R$ {saldo} 😁");
+    }
+}
+
+void Depositar()
+{
+    double deposito;
+
+    if (double.TryParse(Console.ReadLine(), out deposito))
+    {
+        saldo = saldo + deposito;
+    }
+    else
+    {
+        Console.WriteLine("Valor para depósito inválido.");
+    }
+
+    SalvarOperacao(deposito, "deposito");
+
+}
+
+void Sacar()
+{
+    double saque;
+
+    if (double.TryParse(Console.ReadLine(), out saque))
+    {
+        saldo = saldo - saque;
+    }
+    else
+    {
+        Console.WriteLine("Valor para depósito inválido.");
+    }
+
+    SalvarOperacao(saque, "saque");
+}
+
+void SalvarOperacao(double paramDeposito, string operacao)
+{
+    string caminho = "extrato.txt";
+
+    if (File.Exists(caminho))
+    {
+    string conteudo = File.ReadAllText(caminho);
+
+    File.WriteAllText(caminho, conteudo + $"\"{operacao}: R$ {paramDeposito}\",");
+    }
+}
+
+void Extrato()
+{
+    string caminho = "extrato.txt";
+
+    if (File.Exists(caminho))
+    {
+        string conteudo = File.ReadAllText(caminho);
+
+        string[] transacoes = conteudo.Split("\",\"");
+
+        for (int cont = 0; cont < transacoes.Length; cont++)
+        {
+            Console.WriteLine(transacoes[cont]);
+        }
+    }
+}
---
+++ c#- 30.06/hortinha-dos-D/Program.cs
@@ -0,0 +1,169 @@
+﻿int opcao = 0;
+
+do
+{
+    ExibirMenu();
+
+    if (int.TryParse(Console.ReadLine(), out opcao))
+    {
+        switch (opcao)
+        {
+            case 1:
+                CadFruta();
+                Console.ReadKey();
+                break;
+            case 2:
+                CadLegume();
+                Console.ReadKey();
+                break;
+            case 3:
+                CadVerdura();
+                Console.ReadKey();
+                break;
+            default:
+                Console.WriteLine("Escolha uma opção válida");
+                Console.ReadKey();
+                break;
+        }
+    }
+    else
+    {
+        Console.WriteLine("Não é uma opção válida, tente novamente");
+        Console.ReadKey();
+    }
+
+} while (opcao != 3);
+
+void ExibirMenu()
+{
+    Console.Clear();
+    Console.WriteLine("Escolha qual produto gostaria de cadastrar");
+    Console.WriteLine("1 - Frutas");
+    Console.WriteLine("2 - Legumes");
+    Console.WriteLine("3 - Verduras");
+    Console.Write("Escolha uma opção: ");
+}
+
+void CadFruta()
+{
+    Console.WriteLine("Digite a fruta desejada: ");
+    string frutas = Console.ReadLine();
+
+    if (frutas != "")
+    {
+    }
+    else
+    {
+        Console.WriteLine("Fruta inválida");
+    }
+
+    SalvarFrutas(frutas);
+}
+
+void CadLegume()
+{
+    Console.WriteLine("Digite o Legume desejado: ");
+    string legumes = Console.ReadLine();
+
+    if (legumes != "")
+    {
+    }
+    else
+    {
+        Console.WriteLine("Legume inválido");
+    }
+
+    SalvarLegumes(legumes);
+}
+
+void CadVerdura()
+{
+    Console.WriteLine("Digite a Verdura desejada: ");
+    string verduras = Console.ReadLine();
+
+    if (verduras != "")
+    {
+        
+    }
+    else
+    {
+        Console.WriteLine("Verdura inválida");
+    }
+
+    SalvarVerduras(verduras);
+}
+
+void SalvarFrutas(string paramFrutas)
+{
+    string caminho = "frutas.csv";
+
+    if (File.Exists(caminho))
+    {
+        string conteudo = File.ReadAllText(caminho);
+
+        ContemFruta(paramFrutas, conteudo, caminho);
+    }
+}
+
+void ContemFruta(string paramFrutas1, string paramConteudo, string paramCaminho)
+{
+    if(paramConteudo.Contains(paramFrutas1))
+    {
+        Console.WriteLine("Digite outra fruta, fruta já cadastrada!");
+    }
+    else
+    {
+        Console.WriteLine($"{paramFrutas1} cadastrada com sucesso!");
+        File.WriteAllText(paramCaminho, paramConteudo + $"{paramFrutas1}, ");
+    }
+}
+
+void SalvarLegumes(string paramLegumes)
+{
+    string caminho = "legumes.csv";
+
+    if (File.Exists(caminho))
+    {
+        string conteudo = File.ReadAllText(caminho);
+
+        ContemLegume(paramLegumes, conteudo, caminho);
+    }
+}
+
+void ContemLegume(string paramLegumes1, string paramConteudo, string paramCaminho)
+{
+    if(paramConteudo.Contains(paramLegumes1))
+    {
+        Console.WriteLine("Digite outro legume, legume já cadastrada!");
+    }
+    else
+    {
+        Console.WriteLine($"{paramLegumes1} cadastrado com sucesso!");
+        File.WriteAllText(paramCaminho, paramConteudo + $"{paramLegumes1}, ");
+    }
+}
+
+void SalvarVerduras(string paramVerduras)
+{
+    string caminho = "verduras.csv";
+
+    if (File.Exists(caminho))
+    {
+        string conteudo = File.ReadAllText(caminho);
+
+        ContemVerdura(paramVerduras, conteudo, caminho);
+    }
+}
+
+void ContemVerdura(string paramVerduras1, string paramConteudo, string paramCaminho)
+{
+    if(paramConteudo.Contains(paramVerduras1))
+    {
+        Console.WriteLine("Digite outra verdura, verdura já cadastrada!");
+    }
+    else
+    {
+        Console.WriteLine($"{paramVerduras1} cadastrada com sucesso!");
+        File.WriteAllText(paramCaminho, paramConteudo + $"{paramVerduras1}, ");
+    }
+}
